8e36e5468f0f6fd1dfc8b40888fe7c802c3c261d
Alexandros Kosiaris <akosiaris@uname.gr>
2025-08-19 14:42:57 +0300

    Add deps-new output to setup project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to akosiaris/myexpenses2hledger
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/akosiaris/myexpenses2hledger_test.clj
@@ -0,0 +1,7 @@
+(ns akosiaris.myexpenses2hledger-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [akosiaris.myexpenses2hledger :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
