5940588025fe3cf155f9a501687b9960ad103a39
Alexandros Kosiaris <akosiaris@uname.gr>
2025-08-22 14:47:55 +0300

    Add posting and transaction formatters
    Why:
    We need those to output an entire transcation
    
    What:
    Add the per posting formatter as well as the one for the entirety of a
    transaction, composing the header formatted string followed by the per
    posting formatted strings, separated by newlines
    
    Add passing tests for these as well

--- src/akosiaris/myexpenses2hledger/formatter.clj
+++ src/akosiaris/myexpenses2hledger/formatter.clj
@@ -6,23 +6,20 @@
 
 (def date-format :iso-local-date)
 
-(defn format-transaction
-  "Formats a transaction in the hledger format"
-  [transaction]
-  {:pre [(s/valid? ::spec/transaction transaction)]})
-
 (defn format-posting
   "Formats a transaction's posting in the hledger syntax"
   [posting]
   {:pre [(s/valid? ::spec/posting posting)]}
   (let [ffs [[:status, identity]
              [:account, identity]
-             [:amount, str]
-             [:commodity, #(if (re-matches #" " %) (format "\"%s\"" %) (identity %))]
-             [:comment, #(if % (format " ; %s" %) (identity %))]
-             [:tag, identity]]
+             [:amount, #(format " %s" %)] ; TODO: solve this for proper alignment
+             [:commodity, #(if (re-matches #" " %) (format "\"%s\"" %) (identity %))] ; TODO: solve this for proper alignment
+             ; TODO: This needs more work to reflect both unit price and total price as well as cost commodity
+             [:cost, #(if % (format "@ %s" %) (identity %))] ; TODO: solve this for proper alignment
+             [:comment, #(if % (format " ; %s" %) (identity %))] ; TODO: solve this for proper alignment
+             [:tag, identity]] ; TODO: solve this for proper alignment
         r (mapv #((second %) (get posting (first %))) ffs)]
-    (reduce #(if (blank? %2) %1 (str %1 " " %2)) (repeat 4 " ") r)))
+    (reduce #(if (blank? %2) %1 (str %1 " " %2)) (apply str (repeat 3 " ")) r)))
 
 (defn format-transaction-header
   "Formats the transaction header, that is date, payee, status, code, comment, tag"
@@ -33,8 +30,16 @@
              [:code, #(if % (format "(%s)" %) (identity %))]
              [:payee, identity]
              [:note, #(if % (format "| %s" %) (identity %))]
-             [:comment, #(if % (format " ; %s" %) (identity %))]
-             [:tag, identity]]
+             [:comment, #(if % (format " ; %s" %) (identity %))] ; TODO: solve this for proper alignment
+             [:tag, identity]] ; TODO: solve this for proper alignment
         r (mapv #((second %) (get transaction (first %))) ffs)]
     (reduce #(if (blank? %2) %1 (str %1 " " %2)) r)))
 
+(defn format-transaction
+  "Formats a transaction in the hledger format"
+  [transaction]
+  {:pre [(s/valid? ::spec/transaction transaction)]}
+  (let [header (format-transaction-header transaction)
+        postings (mapv format-posting (:postings transaction))
+        lines (if (empty? postings) [header] (cons header postings))]
+    (clojure.string/join "\n" lines)))
--- test/akosiaris/myexpenses2hledger/formatter_test.clj
+++ test/akosiaris/myexpenses2hledger/formatter_test.clj
@@ -30,5 +30,39 @@
     (let [t1 {:date (jt/local-date "2025-08-20") :payee "no comment" :status "*"}
           t2 {:date (jt/local-date "2025-08-20") :payee "with comment" :comment "a comment" :status "*"}]
       (is (= "2025-08-20 * no comment" (f/format-transaction-header t1)))
-      (is (= "2025-08-20 * with comment  ; a comment" (f/format-transaction-header t2)))))
-  )
+      (is (= "2025-08-20 * with comment  ; a comment" (f/format-transaction-header t2))))))
+
+(deftest proper-postings
+  (testing "Bare minimum postings"
+    (let [p1 {:account "expenses" :amount 1M :commodity "EUR"}
+          p2 {:account "assets" :amount -1M :commodity "$"}]
+      (is (= "    expenses  1 EUR" (f/format-posting p1)))
+      (is (= "    assets  -1 $" (f/format-posting p2)))))
+  (testing "With a status"
+    (let [p1 {:account "expenses" :amount 1M :commodity "EUR" :status "*"}]
+      (is (= "    * expenses  1 EUR" (f/format-posting p1)))))
+  (testing "With a cost"
+    (let [p1 {:account "expenses" :amount 1M :commodity "EUR" :cost 1.5M}]
+      (is (= "    expenses  1 EUR @ 1.5" (f/format-posting p1)))))
+  (testing "With a comment"
+    (let [p1 {:account "expenses" :amount 1M :commodity "EUR" :comment "A comment"}]
+      (is (= "    expenses  1 EUR  ; A comment" (f/format-posting p1))))))
+
+(deftest proper-transactions
+  (testing "Bare minimum transactions"
+    (let [t1 {:date (jt/local-date "2025-08-20") :payee "Pending" :status "!"}
+          t2 {:date (jt/local-date "2025-08-20") :payee "Cleared" :code "123" :status "*"}
+          t3 {:date (jt/local-date "2025-08-20") :payee "Empty string" :code "123" :status ""}
+          ;; t4 {:date (jt/local-date "2025-08-20") :payee "no status" :code "123"}
+          ]
+      (is (= "2025-08-20 ! Pending" (f/format-transaction t1)))
+      (is (= "2025-08-20 * (123) Cleared" (f/format-transaction t2)))
+      (is (= "2025-08-20 (123) Empty string" (f/format-transaction t3)))))
+  (testing "Transactions with postings"
+    (let [p1 {:account "expenses" :amount 1M :commodity "EUR"}
+          p2 {:account "assets" :amount -1M :commodity "$"}
+          t1 {:date (jt/local-date "2025-08-20")
+              :payee "Pending"
+              :status "!"
+              :postings [p1, p2]}]
+      (is (= "2025-08-20 ! Pending\n    expenses  1 EUR\n    assets  -1 $" (f/format-transaction t1))))))
