6d3999b0c7942f33641bcae4c09a3a8bbd268eed
Alexandros Kosiaris <akosiaris@uname.gr>
2025-08-25 17:02:11 +0300

    Add first draft of the entrypoint
    Why:
    We want to be able to call this
    
    What:
    Add options, option parsing and validation and logging setup

--- src/akosiaris/myexpenses2hledger.clj
+++ src/akosiaris/myexpenses2hledger.clj
@@ -1,5 +1,66 @@
 (ns akosiaris.myexpenses2hledger
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as string]
+            [akosiaris.myexpenses2hledger.logsetup :refer [setup-logging]]
+            [com.brunobonacci.mulog :as m])
   (:gen-class))
+
+(def ^:private VERSION "0.0.1")
+
+(def ^:private cli-options
+  [;; output file
+   ["-o" "--output FILE" "Path to output" :missing "Output file required"]
+   ;; input. Can be a file or a directory
+   ["-i" "--input FILE" "Path to input. Can be file or directory" :missing "Input file/directory required"]
+   ;; Logging scheme
+   [nil "--ecs-logging" "Use Elastic Common Schema logging. Useful if structured logging is required"]
+   ["-v" "--verbose" "Verbosity level. May be specific multiple times to increase level"
+    :default 0
+    :update-fn inc]
+   ["-h" "--help" "This help"]
+   [nil "--version" "Display version and exit"]])
+
+(defn- usage
+  "Returns the summary as a usage"
+  [options-summary]
+  (let [text ["Parses:"
+              "* A single account MyExpenses JSON export"
+              "* A merged accounts MyExpenses JSON export"
+              "* A directory with multiple single account MyExpenses JSON exports"
+              "and produces an hledger compatible journal."
+              options-summary]]
+    (string/join \newline text)))
+
+(defn- error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn- exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn validate-args
+  "Validate the command line arguments. Return a map. In case of the program needing to exit, provide an exit message. Otherwise, the options provided"
+  [args]
+  (let [{:keys [options errors summary]} (parse-opts args cli-options)]
+    (cond
+      ;; First handle the help option
+      (:help options) ; help => exit OK with usage
+      {:exit-message (usage summary) :ok? true}
+      ;; version output
+      (:version options) ; version => exit OK with version string
+      {:exit-message VERSION :ok? true}
+      ;; argument/option parsing errors
+      errors ; exit with errors
+      {:exit-message (error-msg errors)}
+      ;; Input/Output type validation
+      (nil? (:input options))
+      {:exit-message "Missing input path"}
+      (nil? (:output options))
+      {:exit-message "Missing output path"}
+      ;; Everything checks out scenario
+      :else
+      {:options options})))
 
 (defn greet
   "Callable entry point to the application."
@@ -7,6 +68,11 @@
   (println (str "Hello, " (or (:name data) "World") "!")))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Main function"
   [& args]
-  (greet {:name (first args)}))
+  (let [{:keys [options exit-message ok?]} (validate-args args)]
+    (when exit-message
+      (exit (if ok? 0 1) exit-message))
+    ;; Setup logging
+    (setup-logging (assoc options :version VERSION))
+    (exit 1 (str "Not implemented yet. Options: " options))))
